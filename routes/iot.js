eacf3244644c778d891612938a703cac6c12ad4b
Phil Estes <estesp@gmail.com>
2014-06-04 13:02:10 -0400

    add call to IoT API

--- routes/iot.js
+++ routes/iot.js
@@ -2,11 +2,17 @@
 var env = JSON.parse(process.env.VCAP_SERVICES);
 var iotprops = getEnv(env, 'Wearable-');
 
-exports.getDevice = function(req, res) {
+var querystring = require('querystring');
+var https = require('https');
+var url = require('url');
+
+exports.getDeviceData = function(req, res) {
     var user = req.body.username;
     var pw = req.body.pw;
     console.log("Username: "+user);
-    
+    console.log("Iot app id: "+iotprops.appId);
+    console.log("Iot URL: "+iotprops.url);
+    queryFitbitData(user, pw, '2014-05-16', updateDataView);
     res.render('iotview');
 };
 
@@ -18,4 +24,42 @@ function getEnv(vcapEnv, serviceNameStr) {
               return serviceBindingData.credentials;
         }
     }
+}
+
+function updateDataView(respObj) {
+
+}
+
+function queryFitbitData(username, password, datestr, callbackFn) {
+
+  var iotURLObj = url.parse(iotprops.url);
+  var host = iotURLObj.host;
+  var authStr = username+":"+password;
+  var endpoint = "/iot/doc?id=fb_activity_"+datestr+"&appId="+iotprops.appId;
+
+  var options = {
+    host: host,
+    path: endpoint,
+    method: "GET",
+    auth: authStr
+  };
+
+    var req = https.request(options, function(res) {
+    res.setEncoding('utf-8');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+
+    res.on('end', function() {
+      console.log(responseString);
+      var responseObject = JSON.parse(responseString);
+      callbackFn(responseObject);
+    });
+  });
+
+  req.write(dataString);
+  req.end();
 }
